ee478f8ba97f85438398cd8e1bbe9a80de6ba4c2
Julio Cesar <jc.imp10@gmail.com>
2024-04-11 15:02:54 +0100

    feat: first section with effects

--- src/scripts.js
+++ src/scripts.js
@@ -1,26 +1,82 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+  // Texto inicial
+  const initialText = `At first, he was just a young man who kept his ideas stuck in his head, as in the darkness of a cave. Later, with the help of <span class="duration-500 hover:text-white transition">Philosophy</span> and <span class="duration-500 hover:text-white transition">Natural Sciences</span>, a light came, but it was the <span class="CODE-transition">CODE</span> that changed everything.`;
+
+  // Define o conteúdo do contêiner de texto com o texto inicial
+  const textContainer = document.getElementById("textContainer");
+  textContainer.innerHTML = initialText;
+
+  // Adiciona classe "text-white" à palavra "CODE" quando o usuário passar o mouse sobre ela
   const codeWord = document.querySelector(".CODE-transition");
-  codeWord.addEventListener("mouseover", () => {
-    codeWord.classList.add("text-white");
-  });
+  if (codeWord) {
+    codeWord.addEventListener("mouseover", () => {
+      codeWord.classList.add("text-white", "font-bold");
+      textContainer.classList.add("parent-container");
+    });
+  }
 
   // Adiciona classe "text-white" à palavra "CODE" quando o usuário rolar para baixo
   window.addEventListener("scroll", function () {
-    if (window.scrollY > 25) {
-      codeWord.classList.add("text-white");
-      otherWords.forEach((word) => word.classList.remove("text-white"));
+    const otherWords = document.querySelectorAll(".transition");
+
+    if (window.scrollY > 15) {
+      codeWord.classList.add("text-white", "font-bold");
+       textContainer.classList.add("parent-container");
+      otherWords.forEach((word) => {
+        word.classList.remove("text-white", "font-bold");
+      });
+    } else {
+      otherWords.forEach((word) => {
+        word.classList.add("text-white","font-bold");
+      });
     }
   });
 
-});
+  // Adiciona efeito de digitação
+  const typingText = [
+    "curious",
+    "positive",
+    "empathic",
+    "resilient",
+    "developer",
+  ];
+  const typingEffectElement = document.getElementById("typingEffect");
 
-const scrollButtons = document.querySelectorAll(".scroll-btn");
-scrollButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const targetSelector = button.getAttribute("data-scroll-to");
-    const target = document.querySelector(targetSelector);
-    if (target) {
-      target.scrollIntoView({ behavior: "smooth" });
-    }
-  });
+  function startTyping(index) {
+    let i = 0;
+    const interval = setInterval(function () {
+      typingEffectElement.textContent += typingText[index][i];
+      i++;
+      if (i === typingText[index].length) {
+        clearInterval(interval);
+        setTimeout(function () {
+          startDeleting(index);
+        }, 1000);
+      }
+    }, 300);
+  }
+
+  function startDeleting(index) {
+    let i = typingText[index].length - 1;
+    const interval = setInterval(function () {
+      typingEffectElement.textContent = typingEffectElement.textContent.slice(
+        0,
+        -1
+      );
+      i--;
+      if (i === -1) {
+        clearInterval(interval);
+        setTimeout(function () {
+          if (index < typingText.length - 1) {
+            startTyping(index + 1);
+          } else {
+            startTyping(0);
+          }
+        }, 500);
+      }
+    }, 100);
+  }
+
+  startTyping(0);
 });
