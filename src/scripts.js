beeaad6d1187c5fa95d35e96884f66f2eca4267a
Julio Cesar <julio.pereira.t0126213@edu.atec.pt>
2024-04-12 14:56:18 +0100

    feat: buttons and animations

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,6 @@
+//Inicialização do texto e efeitos de rolagem:
 document.addEventListener("DOMContentLoaded", function () {
-
-  // Texto inicial
   const initialText = `At first, he was just a young man who kept his ideas stuck in his head, as in the darkness of a cave. Later, with the help of <span class="duration-500 hover:text-white transition">Philosophy</span> and <span class="duration-500 hover:text-white transition">Natural Sciences</span>, a light came, but it was the <span class="CODE-transition">CODE</span> that changed everything.`;
-
   // Define o conteúdo do contêiner de texto com o texto inicial
   const textContainer = document.getElementById("textContainer");
   textContainer.innerHTML = initialText;
@@ -20,63 +18,99 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("scroll", function () {
     const otherWords = document.querySelectorAll(".transition");
 
-    if (window.scrollY > 15) {
+    if (window.scrollY > 25) {
       codeWord.classList.add("text-white", "font-bold");
-       textContainer.classList.add("parent-container");
+      textContainer.classList.add("parent-container");
       otherWords.forEach((word) => {
         word.classList.remove("text-white", "font-bold");
       });
     } else {
       otherWords.forEach((word) => {
-        word.classList.add("text-white","font-bold");
+        word.classList.add("text-white", "font-bold");
       });
     }
   });
+});
 
-  // Adiciona efeito de digitação
-  const typingText = [
-    "curious",
-    "positive",
-    "empathic",
-    "resilient",
-    "developer",
+document.addEventListener("DOMContentLoaded", async function () {
+  try {
+    // Faz a requisição para a API para obter a foto de perfil
+    const response = await fetch("https://api.github.com/users/JulioCesar-X");
+    const data = await response.json();
+
+    // Obtém a URL da foto de perfil
+    const profileImageUrl = data.avatar_url;
+
+    // Define a imagem de fundo do container redondo com a URL da foto de perfil
+    const container = document.querySelector(".container");
+    container.style.backgroundImage = `url('${profileImageUrl}')`;
+  } catch (error) {
+    console.error("Erro ao obter a foto de perfil:", error);
+  }
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Função para aplicar a animação de movimento aleatório nos elementos
+  function randomValues() {
+    anime({
+      targets: ".btn-magnetic",
+      translateX: function () {
+        return anime.random(-2000, 2000); // Movimento aleatório na horizontal
+      },
+      translateY: function () {
+        return anime.random(-2000, 2000); // Movimento aleatório na vertical
+      },
+      rotate: function () {
+        return anime.random(0, 360); // Rotação aleatória
+      },
+      easing: "linear",
+      duration: 800,
+      complete: randomValues,
+    });
+  }
+
+  // Inicia a animação de movimento aleatório
+  randomValues();
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const vectorWords = [
+    "Curious",
+    "Passionate",
+    "Creative",
+    "Persistent",
+    "Developer",
   ];
-  const typingEffectElement = document.getElementById("typingEffect");
+  const typingEffect = document.getElementById("typingEffect");
+  let index = 0;
 
-  function startTyping(index) {
-    let i = 0;
-    const interval = setInterval(function () {
-      typingEffectElement.textContent += typingText[index][i];
-      i++;
-      if (i === typingText[index].length) {
-        clearInterval(interval);
-        setTimeout(function () {
-          startDeleting(index);
-        }, 1000);
+  function typeNextWord() {
+    if (index < vectorWords.length) {
+      const word = vectorWords[index];
+      typingEffect.textContent = ""; // Limpa o conteúdo do elemento
+      let i = 0;
+
+      // Função para adicionar caracteres com efeito de digitação e caractere piscante
+      function typeCharacter() {
+        if (i <= word.length) {
+          typingEffect.textContent = word.substring(0, i) + "|"; // Adiciona o texto digitado até o momento e o caractere piscante
+          i++;
+          setTimeout(typeCharacter, 300); // Tempo de digitação entre os caracteres
+        } else {
+          setTimeout(removeLastCharacter, 400); // Tempo antes de remover o caractere piscante
+        }
       }
-    }, 300);
+
+      // Função para remover o último caractere piscante
+      function removeLastCharacter() {
+        typingEffect.textContent = word.substring(0, i - 1); // Remove o último caractere piscante
+        index++;
+        setTimeout(typeNextWord, 600); // Tempo de espera antes de passar para a próxima palavra
+      }
+
+      typeCharacter(); // Inicia a função para digitar os caracteres
+    }
   }
 
-  function startDeleting(index) {
-    let i = typingText[index].length - 1;
-    const interval = setInterval(function () {
-      typingEffectElement.textContent = typingEffectElement.textContent.slice(
-        0,
-        -1
-      );
-      i--;
-      if (i === -1) {
-        clearInterval(interval);
-        setTimeout(function () {
-          if (index < typingText.length - 1) {
-            startTyping(index + 1);
-          } else {
-            startTyping(0);
-          }
-        }, 500);
-      }
-    }, 100);
-  }
-
-  startTyping(0);
+  typeNextWord(); // Inicia a sequência de digitação
 });
